1da6018eb7a8ae682623f01fc6ce53985eeb94aa
Jahaziel <jahaziel.vaz@gmail.com>
2019-10-13 13:52:37 -0600

    new codeRecycle section with examples

--- app.js
+++ app.js
@@ -11,5 +11,6 @@ let todoController = require('./controllers/todoController')
 todoController(app)
 
 // Server
-app.listen(3000);
-console.log('Listening to port 3000');
+let port = 3000;
+app.listen(port);
+console.log(`Listening to port ${port}`);
